26ca90ae9b74be4f4f27afa4a137ac9d010bbd6d
adevlinb <devlinbgenassembly@gmail.com>
2022-03-14 15:29:24 -0700

    more post card css updates

--- src/components/DisplayPostCard/DisplayPostCard.jsx
+++ src/components/DisplayPostCard/DisplayPostCard.jsx
@@ -30,36 +30,36 @@ export default function DisplayPostCard({post, addComment}) {
         {cardFlip ?
             <div className="postCard">
                 <div className='postRow'>
-                    <div className="image">
-                        {post && (post.book.thumbnail ?
-                            <>
-                                {post && <h3> {post.user.name}</h3>}
-                                {post && <p> {new Date(post.createdAt).toLocaleDateString()}</p>}
-                                <img src={`${post.book.thumbnail}`} alt={`${post.book.title}`} className="apiImage" name="thumbnail" />
-                                <button onClick={() => setcardFlip(!cardFlip)}>Details</button>
-                            </>
-                            :
-                            <>
-                                    {post && <h3> {post.user.name} </h3>}
-                                <img src="http://i.imgur.com/J5LVHEL.jpg" alt={`${post.book.title}`} name="thumbnail" />
-                                <button onClick={() => setcardFlip(!cardFlip)}>Details</button>
-                            </>
-                        )}
+                    <div className="textRow">
+                        {/* <div className="personIcon"> */}
+                            <i class="material-icons" id="personIconProfile">account_circle</i>
+                        {/* </div> */}
+                        {post && <h5> {post.user.name}</h5>}
+                        {post && <h5> {new Date(post.createdAt).toLocaleDateString()}</h5>}
+                            <i class="material-icons" id="personIconBook">menu_book</i>
                     </div>
-                    <div className="text">
-                        {post && post.book.title ? <div name="Title" value={`${post.book.title}`} >{post.book.title}</div> : <div>N/A</div>}
-                        {post && post.book.authors ? <div name="Authors" value={`${post.book.authors}`}>{post.book.authors}</div> : <div>N/A</div>}
-                        {post && post.book.subjects ? <div name="Subjects" value={`${post.book.subjects}`}>{post.book.subjects}</div> : <div>N/A</div>}
-                        {post && post.book.publishers ? <div name="Publishers">{post.book.publishers}</div> : <div>N/A</div>}
-                        {post && post.book.pageCount ? <div name="pageCount">{post.book.pageCount}</div> : <div>N/A</div>}
-                        {post && post.book.isbnNum ? <div name="isbnNum">{post.book.isbnNum}</div> : <div>N/A</div>}
-                        {post && post.book.rating ? <div name="rating">{post.book.rating}</div> : <div>N/A</div>}
-                  
+                    <div id="imageDescBox">
+                        <div className="image">
+                            {post && (post.book.thumbnail ?
+                                <>
+                                    <img src={`${post.book.thumbnail}`} alt={`${post.book.title}`} className="apiImage" name="thumbnail" />
+                                    {/* {post && post.book.title ? <h5 name="Title" value={`${post.book.title}`} >{post.book.title}</h5> : <h5>N/A</h5>} */}
+                                </>
+                                :
+                                <>
+                                    <img src="http://i.imgur.com/J5LVHEL.jpg" alt={`${post.book.title}`} name="thumbnail" />
+                                </>
+                            )}
+                        </div>
+                        <div className="description">
+                            {post && post.book.title ? <h5 name="Title" value={`${post.book.title}`} >{post.book.title}</h5> : <h5>N/A</h5>}
+                            {post && <div name="description">{post.description}</div>}
+                        </div>
                     </div>
                 </div>
                 <div className="commentForm">
                     <form onSubmit={(evt) => handleAddComment(`${post._id}`, evt)}>
-                            <input name="content" value={commentData.content} onChange={handleChange} type="text" placeholder='Add Comment!'/>
+                            <input id="commentInput" name="content" value={commentData.content} onChange={handleChange} type="text" placeholder='Add Comment!'/>
                             <button id="commentButton" type="submit">+</button>
                     </form>
                 </div>
@@ -71,12 +71,24 @@ export default function DisplayPostCard({post, addComment}) {
             
             <div className="postCardTwo">
                 
-                {post && <div> {post.user.name} </div>}
-                {post && <div name="description">{post.description}</div>}
-                <button onClick={() => setcardFlip(!cardFlip)}>Return</button>
+                {/* {post && <div> {post.user.name} </div>} */}
+                {/* {post && <div name="description">{post.description}</div>} */}
+                {/* <button onClick={() => setcardFlip(!cardFlip)}>Return</button> */}
             </div>
         }
 
     </>
     );
 }
+
+
+// { post && post.book.authors ? <div name="Authors" value={`${post.book.authors}`}>{post.book.authors}</div> : <div>N/A</div> }
+// { post && post.book.subjects ? <div name="Subjects" value={`${post.book.subjects}`}>{post.book.subjects}</div> : <div>N/A</div> }
+// { post && post.book.publishers ? <div name="Publishers">{post.book.publishers}</div> : <div>N/A</div> }
+// { post && post.book.pageCount ? <div name="pageCount">{post.book.pageCount}</div> : <div>N/A</div> }
+// { post && post.book.isbnNum ? <div name="isbnNum">{post.book.isbnNum}</div> : <div>N/A</div> }
+// { post && post.book.rating ? <div name="rating">{post.book.rating}</div> : <div>N/A</div> }
+
+{/* <button onClick={() => setcardFlip(!cardFlip)}>Details</button> */ }
+
+{/* <button onClick={() => setcardFlip(!cardFlip)}>Details</button> */ }
